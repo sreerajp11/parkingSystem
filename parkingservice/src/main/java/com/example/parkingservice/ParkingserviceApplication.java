9f48fb443680b29253d001ff46881dc7ffd6783a
Sree Raj <psreeraj.43@gmail.com>
2025-07-14 13:46:51 +0530

    Parking service initial setup

---
+++ parkingservice/src/main/java/com/example/parkingservice/ParkingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingserviceApplication.class, args);
+	}
+
+}
